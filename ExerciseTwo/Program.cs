92c01f93231cb76d3c9b590cac7f85de0b04888d
Алексей Телепнев <alekseytelepnev@MacBook-Pro-Aleksej.local>
2022-04-28 02:07:43 +1100

    Add Exercise Two

---
+++ ExerciseTwo/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран
+
+
+void FillArray(int[] Array)
+{
+    int length = Array.Length;
+    int index = 0;
+    while(index < length)
+    {
+        Array[index] = new Random().Next(0, 2);
+        index++;
+    }
+}
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    int position = 0;
+    while(position < count)
+    {
+        Console.Write($" {array[position]} ");
+        position++;
+    }
+}
+
+int[] newArray = new int[8];
+FillArray(newArray);
+PrintArray(newArray);
+
