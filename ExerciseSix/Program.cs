b4949bcee0fa326d31ffb90fc35d2ed9206e7211
Алексей Телепнев <alekseytelepnev@MacBook-Pro-Aleksej.local>
2022-04-29 04:12:02 +1100

    Add Exercise Six

---
+++ ExerciseSix/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задать массив, заполнить случайными положительными трёхзначными числами. 
+// Показать количество нечетных\четных чисел
+
+
+void FillArray(int[] Array)
+{
+    int length = Array.Length;
+    int index = 0;
+    while(index < length)
+    {
+        Array[index] = new Random().Next(100, 1000);
+        index++;
+    }
+}
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    int position = 0;
+    while(position < count)
+    {
+        Console.Write($" {array[position]} ");
+        position++;
+    }
+}
+
+
+int[] newArray = new int[10];
+FillArray(newArray);
+PrintArray(newArray);
+
+
+int l = newArray.Length;
+int even = 0;
+int odd = 0;
+int i = 0;
+
+while(i < l)
+{
+    if(newArray[i] % 2 == 0) even++;
+    else odd++;
+    i++;
+}
+
+
+Console.WriteLine();
+Console.WriteLine($" четных - {even} нечетных - {odd} ");
+
