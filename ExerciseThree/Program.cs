ca68d8f0c18d2f5b3e34894b9575c1e36fb46bc6
Алексей Телепнев <alekseytelepnev@MacBook-Pro-Aleksej.local>
2022-04-28 22:08:38 +1100

    Add Exercise Three

---
+++ ExerciseThree/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задать массив из 12 элементов, заполненных числами из [0,9]. 
+// Найти сумму положительных/отрицательных элементов массива
+
+
+void FillArray(int[] Array)
+{
+    int length = Array.Length;
+    int index = 0;
+    while(index < length)
+    {
+        Array[index] = new Random().Next(0, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    int position = 0;
+    while(position < count)
+    {
+        Console.Write($" {array[position]} ");
+        position++;
+    }
+}
+
+void SumArray(int[] ourArray)
+{
+    int number = ourArray.Length;
+    int sum = 0;
+    for(int i = 0; i < number; i++)
+    {
+        if(ourArray[i] > 0)
+        {
+            sum = sum + ourArray[i];
+            Console.WriteLine(sum);
+        }
+            if(ourArray[i] < 0)
+            {
+                Console.WriteLine("-1");
+            }
+    }
+}
+int[] newArray = new int[12];
+FillArray(newArray);
+PrintArray(newArray);
+SumArray(newArray);
+
+
