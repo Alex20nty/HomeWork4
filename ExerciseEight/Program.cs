f0819573485a9e505f7a9684988713b9f5d1e9a4
Алексей Телепнев <alekseytelepnev@MacBook-Pro-Aleksej.local>
2022-04-29 22:28:21 +1100

    Add Exercise Eight

---
+++ ExerciseEight/Program.cs
@@ -0,0 +1,45 @@
+﻿// Найти сумму чисел одномерного массива стоящих на нечетной позиции
+
+void FillArray(int[] Array)
+{
+    int length = Array.Length;
+    int index = 0;
+    while(index < length)
+    {
+        Array[index] = new Random().Next(0, 100);
+        index++;
+    }
+}
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    int position = 0;
+    while(position < count)
+    {
+        Console.Write($" {array[position]} ");
+        position++;
+    }
+}
+
+int[] newArray = new int[10];
+FillArray(newArray);
+PrintArray(newArray);
+
+int SumOdd(int[] newArray)
+{
+    int length = newArray.Length;
+    int sum = 0;
+    for(int i = 0; i < length; i++)
+    {
+        if(i % 2 == 0)
+        {
+            sum = sum + newArray[i];
+        }
+    }
+    return sum;
+}
+
+int result = SumOdd(newArray);
+Console.WriteLine();
+Console.WriteLine("Сумма нечетных чисел массива = " +result);
