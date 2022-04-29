2654c6a52d3f01dbe4b509f761da6c9fca0eb2db
Алексей Телепнев <alekseytelepnev@MacBook-Pro-Aleksej.local>
2022-04-29 21:51:44 +1100

    Add Exercise Seven

---
+++ ExerciseSeven/Program.cs
@@ -0,0 +1,16 @@
+﻿// В одномерном массиве из 123 чисел найти количество элементов из отрезка [10,99]
+
+int[] newArray = new int[123];
+
+int NumberOf(int[] newArray)
+{
+    int num = 0;
+    for(int i = 10; i < 99; i++)
+    {
+        num = num + 1;
+    }
+    return num;
+}
+
+int result = NumberOf(newArray);
+System.Console.WriteLine(result);
